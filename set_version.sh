66ea98d9a69744b5d83303e26cb2009f34cbab22
Dan Boitnott <dboitnot@gmail.com>
2022-05-26 06:52:34 -0500

    CI update Thu May 26 06:52:34 AM CDT 2022

--- set_version.sh
+++ set_version.sh
@@ -15,8 +15,6 @@ ver="$(echo "$ref" |grep -Eo '[0-9]+\.[0-9]+(\.[0-9]+)?$' || echo SNAPSHOT)"
 >&2 echo "Updating configure.ac"
 sed -ie "s/^AC_INIT(.*/AC_INIT([skiring], [${ver}])/" configure.ac || die "failed to update configure.ac"
 
->&2 echo "Generating configure and Makefile.in"
-autoconf || die "autoconf failed"
-automake || die "automake failed"
+>&2 echo "REMBER TO RE-RUN autoconf AND automake"
 
 echo "$ver"
